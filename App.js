9ae883e16ef2b1dd3216052b3279e0ef07c63013
slee2769 <slee2769@uwo.ca>
2023-04-01 02:53:01 -0400

    worked on home page and tried inputting fonts

--- App.js
+++ App.js
@@ -11,18 +11,21 @@ import pic5 from '../AdaTeam3/images/pic5.jpg';
 import pic6 from '../AdaTeam3/images/pic6.jpg';
 import pic7 from '../AdaTeam3/images/pic7.jpg';
 import pic8 from '../AdaTeam3/images/pic8.jpg';
+import search from '../AdaTeam3/images/search.jpg';
 
 export default function App() {
   return (
   <View style = {styles.container}>
     <View> 
       <View style={styles.rectangle}></View>
-      <Text style={styles.search}>search</Text>
+      <Text style={styles.t1}>search</Text>
       <View style={styles.secondRectangle}></View>
       <Text style={styles.banner}>featured banner</Text>
       <Text style={styles.recommended}>Recommended for you</Text>
       <Text style={styles.popular}>Popular right now</Text>
       <View style={styles.menu}></View>
+
+
       <Image
         source={home}
         style={styles.home}
@@ -35,7 +38,7 @@ export default function App() {
         source={save}
         style={styles.save}
         />
-      <ScrollView horizontal = {true}>
+      <ScrollView style={styles.scroll1} horizontal = {true}>
       <Image
         source={pic1}
         style={styles.pic1}
@@ -53,7 +56,7 @@ export default function App() {
         style={styles.pic4}
         />
       </ScrollView>
-      <ScrollView horizontal = {true}>
+      <ScrollView style={styles.scroll2} horizontal = {true}>
       <Image
         source={pic5}
         style={styles.pic5}
@@ -71,6 +74,10 @@ export default function App() {
         style={styles.pic8}
         />
       </ScrollView>
+      <Image
+      source={search}
+      style={styles.search}
+      />
       <StatusBar style="auto" />
     </View>
   </View>
@@ -89,42 +96,69 @@ const styles = StyleSheet.create({
     width: 270,
     backgroundColor: '#664739',
     borderRadius: 16,
-   
+    position: 'relative',
+    left: 50,
+    top: 25
   },
-  search: {
-    color:'white',
+  t1: {
+    color: '#EBE2DD',
+    position : 'relative',
+    right: -90,
+    bottom: -1
+    
     
   },
   secondRectangle: {
     height: 150,
-    width: 360,
+    width: 375,
     backgroundColor: '#D9D9D9',
-    
+    position: 'relative',
+    bottom: -50
   },
   banner: {
     color: '#000000',
+    position: 'relative',
+    top: -10,
+    fontFamily: 'Roboto-Regular',
   },
   recommended: {
-    color: '#000000'
+    color: '#000000',
+    position: 'relative',
   },
   popular: {
-    color: '#000000'
+    color: '#000000',
+    position: 'relative',
 
   },
   menu: {
     height: 80,
     width: 375,
-    backgroundColor: '#664739'
+    backgroundColor: '#664739',
+    position: 'relative',
+    top: 350
   },
   home: {
     position: 'relative',
     bottom: 50
   },
   camera: {
+    position: 'relative',
 
   },
   save: {
+    position: 'relative',
     
+  },
+  search: {
+    position: 'relative',
+    left: 57,
+    bottom: 615
+
+  }, 
+  scroll1: {
+    position : 'relative',
+    top: 30,
   }
+
   
 });
